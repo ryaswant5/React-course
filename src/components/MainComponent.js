33cc9abb879c6faddb8ddf680d51f6efdd308820
Rachumallu Yaswant <alextran725@yahoo.com>
2020-07-02 20:25:31 +0530

    Single Page Applications Part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponents';
 import DishDetail from './Dishdetailcomponent';
+import Contact from './ContactComponent';
 import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -14,28 +18,36 @@ class Main extends Component {
     super(props);
     this.state={
       dishes: DISHES,
-      selectedDish:null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
+
   onDishSelect(dishId)
   {
           this.setState({selectedDish:dishId});        
   }
+
   render() {
 
     const HomePage =() => {
       return(
-        <Home />
+        <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
 
     return (
       <div>
-
         <Header/>
         <Switch>
         <Route path ='/home' component={HomePage}/>
         <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes}/> }/>
+        <Route exact path='/contactus' component={Contact} />
         <Redirect to="/home"/>
         </Switch>
         <Footer/>
